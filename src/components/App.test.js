7064d73c065cd30a72774e77de62ae5690b8eb2a
Alejandro Mansor <amansor@gmail.com>
2019-07-08 17:09:03 -0300

    testUtils aux functions created to be called by test without duplicanting code

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../test/testUtils';
 import App from './App';
 
 test('renders without error', () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import { findByTestAttr } from '../../test/testUtils';
 import Header from './Header';
 
 const setup = (props={}, state=null) => {
   return shallow(<Header {...props} />)
-}
-
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
 }
 
 test('renders without error', () => {
